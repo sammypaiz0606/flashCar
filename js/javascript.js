87e27f6930783261f5f1af878272bd71451b8bd7
Your Name <youremail@yourdomain.com>
2022-07-21 22:16:46 -0700

    slide effect

--- js/javascript.js
+++ js/javascript.js
@@ -4,11 +4,15 @@ let lostBtn2 = document.querySelector('.lostBtn2');
 let lostBtn3 = document.querySelector('.lostBtn3');
 
 lostBtn2.addEventListener('click', () => {
-    lostEM_2.style.display = 'block';
+    lostEM_2.style.width = '100%';
+    // lostEM_2.style.display = 'block';
+    lostEM_2.style.visibility = 'visible';
+
     console.log('it worked!');
 });
 
 lostBtn3.addEventListener('click', () => {
-    lostEM_2.style.display = 'none';
+    lostEM_2.style.width = '0px';
+    // lostEM_2.style.visibility = 'hidden';
     console.log('reset workd!');
 });
