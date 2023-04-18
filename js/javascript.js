9f71c90793398594d02525cd2b7fe97c11274e6e
Your Name <youremail@yourdomain.com>
2023-04-18 09:37:42 -0700

    click button for supportInfo

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,9 @@ let lostBtn_1 = document.querySelector('.lostBtn_1');
 let lostEM_2 = document.querySelector('.lostEM_2');
 let page_1 = document.querySelector('.page_1');
 let resetButton = document.querySelector('.resetButton');
+let supportInfo = document.querySelector('.supportInfo');
+
+let support = document.getElementById('support');
 
 lostBtn_1.addEventListener('click', ()=> {
     lostEM_2.style.visibility = 'visible';
@@ -13,4 +16,9 @@ lostBtn_1.addEventListener('click', ()=> {
 resetButton.addEventListener('click', ()=> {
     lostEM_2.style.width = '0';
     page_1.style.visibility = 'visible';
+})
+
+support.addEventListener('click', ()=> {
+    supportInfo.classList.toggle('showSupport'); 
+    // document.body.style.backgroundColor = '#000';
 });
